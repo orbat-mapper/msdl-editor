cacb701632bf37c984fadc5a83958af145c2172f
KMF <kjellmf@gmail.com>
2025-07-03 07:14:50 +0200

    test: Trigger fixit error

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { RouterView } from "vue-router";
-import { Toaster } from "vue-sonner";
+import {onMounted } from "vue";
+import {RouterView} from "vue-router";
+import {Toaster} from "vue-sonner";
 import { useEntityTypeStore } from "./stores/entityTypeStore";
 
 const entityTypeStore = useEntityTypeStore();
 
-onMounted(() => entityTypeStore.init());
+onMounted(()  =>  entityTypeStore.init());
 </script>
 
 <template>
   <RouterView />
-  <Toaster class="pointer-events-auto" />
+  <Toaster class=" pointer-events-auto" />
 </template>
